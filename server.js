3b0348918b8ed001389c4ae1f5cb019065128137
Scott Mosher <scott@scottmosherphotography.com>
2021-02-02 16:54:44 -0700

    modified file

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+'use strict';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const path = require('path');
+const express = require('express');
+const http = require('http');
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const app = express();
+const server = http.Server(app);
+
+const port = process.env.PORT || 8080;
+
+app.use('/', express.static(path.join(__dirname, 'testheroku')));
+
+server.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}/`);
+});
