c0ddc396479b58eba4ac979c0d158781661ca3df
Scott Mosher <scott@scottmosherphotography.com>
2021-02-02 17:59:24 -0700

    added authentication back in

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use('/', express.static(path.join(__dirname, 'testheroku')));
 
 // Handling the actual requests
 // Add checkAuthentication for any endpoint you want users to be authorized to access
-app.use('/clients', clientRoutes)
+app.use('/clients', checkAuthentication, clientRoutes)
 
 // Default
 app.get("/*", function(request, response) {
