a41534045322ebd3dff9c56300f4c5c87fc17e54
Scott Mosher <scott@scottmosherphotography.com>
2021-02-02 17:11:30 -0700

    Added misc files

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const http = require('http');
 const app = express();
 const server = http.Server(app);
 
+// Set paths
+app.use(express.static(path.join(__dirname, 'public')));
+
 const port = process.env.PORT || 8080;
 
 app.use('/', express.static(path.join(__dirname, 'testheroku')));
 
 app.get('/', function(request, response) {
-    response.send("Hi there muddy waters");
+    response.sendFile(__dirname + "/public/sitedetails.html")
 })
 
 server.listen(port, () => {
